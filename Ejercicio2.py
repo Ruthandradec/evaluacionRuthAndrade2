52df6130a771496548def890242ffaceb95f2552
unknown <ruthelenaandrade@gmail.com>
2023-11-01 13:12:50 -0500

    Se creo la funcion para revisar si un movimiento de alfil es valido

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -4,6 +4,15 @@ def convertir_coordenadas(coordenada):
     fila = int(coordenada[1]) - 1
     return fila, columna
 
+def es_movimiento_valido(posicion_inicio, posicion_final):
+    fila_inicio, columna_inicio = posicion_inicio
+    fila_final, columna_final = posicion_final
+
+    # Verificar si la diferencia entre las filas y las columnas es la misma para diagonales
+    if abs(fila_final - fila_inicio) == abs(columna_final - columna_inicio):
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
 
